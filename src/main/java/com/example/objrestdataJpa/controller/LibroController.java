15e26d41c598ec77c7c9b5968d202b810deadb84
thunder609 <oswaldo-pachco@hotmail.es>
2023-01-09 01:28:23 -0400

    crar ,listar libros

--- src/main/java/com/example/objrestdataJpa/controller/LibroController.java
+++ src/main/java/com/example/objrestdataJpa/controller/LibroController.java
@@ -2,10 +2,14 @@ package com.example.objrestdataJpa.controller;
 
 import com.example.objrestdataJpa.entities.Libro;
 import com.example.objrestdataJpa.repository.LibroRepositoy;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
+
 
 @RestController
 public class LibroController {
@@ -22,8 +26,26 @@ public class LibroController {
         return repository.findAll();
    }
     //Buscar un solo libro por su id
-    /*public  Libro  findByid(Long id){
+    @GetMapping("/api/libro/{id}")
+    //colocar el mismo nombre al parametro
+    public ResponseEntity<Libro> findByid(@PathVariable("id")  Long id){
 
-    }*/
+        Optional <Libro> opt =repository.findById(id);
+        //opcion2
+       // return opt.orElse(null);
+        //opcion1
+       if(opt.isPresent())
+            return   ResponseEntity.ok(opt.get());
+        else
+            return  ResponseEntity.notFound().build();
+    }
+    // cear libro
+    @PostMapping("/api/libros")
+    public Libro craerLibro(@RequestBody Libro libro , @RequestHeader HttpHeaders headers){
+        //guardar libro recibido en parametros desde la base datos
+        System.out.println(headers.get("User-Agent"));
+       return  repository.save(libro);
+
+    }
 
 }
